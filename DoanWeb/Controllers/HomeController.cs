eade2c8c4095ab632b130628cfdd97b934a16fdd
duong <duong@DESKTOP-4UTGV4K>
2023-03-29 12:49:58 +0700

    Add project files.

---
+++ DoanWeb/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoanWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Bac()
+        {
+            ViewBag.Message = "Tin tuc mien Bac";
+
+            return View();
+        }
+
+        public ActionResult Nam()
+        {
+            ViewBag.Message = "Tin tuc mien Nam";
+
+            return View();
+        }
+        public ActionResult Account()
+        {
+            ViewBag.Message = "Tai khoan";
+
+            return View();
+        }
+    }
+}
